4f90145b40fd9e3c9e24d4bd1e6f89cca4979e8f
henry.duong <henry.duong@grasshopperasia.com>
2026-02-01 10:49:10 +0700

    Implement supervisor rpc

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,57 @@
-
-
-
+mod supervisor;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
-    
+    println!("Starting Supervisor RPC Client");
+    let server = supervisor::Server::new("localhost", 9001, Some("user"), Some("123"));
+    let res = server.get_api_version().await?;
+    println!("{:?}", res);
+    // let res = server.get_supervisor_version().await?;
+    // println!("{:?}", res);
+    // let res = server.get_state().await?;
+    // println!("{:?}", res);
+    // let res = server.get_pid().await?;
+    // println!("{:?}", res);
+    // let res = server.clear_log().await?;
+    // println!("{:?}", res);
+    // let res = server.read_log(100, 100).await?;
+    // println!("{:?}", res);
+    // let res = server.shutdown().await?;
+    // println!("{:?}", res);
+    // let res = server.restart().await?;
+    // println!("{:?}", res);
+    // let res = server.get_pid().await?;
+    // println!("{:?}", res);
+    // let res = server.get_all_process_info().await?;
+    // println!("{:?}", res);
+    // let res = server.get_process_info("group_normal:normal1").await?;
+    // println!("{:?}", res);
+    // let res = server.get_all_config_info().await?;
+    // println!("{:?}", res);
+    // let res = server.read_process_stdout_log("group_normal:normal1", 100, 100).await?;
+    // println!("{:?}", res);
+    // let res = server.tail_process_stdout_log("group_normal:normal1", 500, 100).await?;
+    // println!("{:?}", res);
+    // let res = server.start_process("group_normal:normal1", true).await?;
+    // println!("{:?}", res);
+    // let res = server.start_all_processes(true).await?;
+    // println!("{:?}", res);
+    // let res = server.stop_process("group_normal:normal1", true).await?;
+    // println!("{:?}", res);
+    // let res = server.stop_all_processes(true).await?;
+    // println!("{:?}", res);
+    // let res = server.stop_process_group("group_normal", true).await?;
+    // println!("{:?}", res);
+    // let res = server.signal_process("group_normal:normal1", "15").await?;
+    // println!("{:?}", res);
+    // let res = server.signal_process_group("group_normal", "15").await?;
+    // println!("{:?}", res);
+    // let res = server.signal_all_processes( "15").await?;
+    // println!("{:?}", res);
+    // let res = server.send_process_stdin("group_normal:normal1", "TEST TEST HELLOO").await?;
+    // println!("{:?}", res);
+    // let res = server.reload_config().await?;
+    // println!("{:?}", res);
+
     Ok(())
 }
