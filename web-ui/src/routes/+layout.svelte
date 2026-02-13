f45df01c25a8478265af9106a61fcd769cefa46c
hung <dthung1602@gmail.com>
2026-02-13 10:31:10 +0700

    Add web-ui

---
+++ web-ui/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import './layout.css';
+    import favicon from '$lib/assets/favicon.svg';
+    import logo from '$lib/assets/Original_Ferris.svg'
+    import {resolve} from '$app/paths';
+
+    let {children} = $props();
+</script>
+
+<svelte:head>
+    <link rel="icon" href={favicon}/>
+</svelte:head>
+
+<style>
+    img {
+        max-width: 4rem;
+    }
+</style>
+
+<header class="z-50 sticky top-0 border-b border-surface-200-800 bg-surface-50-950/50 backdrop-blur-lg h-(--header-height) flex items-center">
+    <a href="{resolve('/')}" class="container mx-auto px-4 flex items-center justify-between gap-2">
+        <img src="{logo}" alt="Logo" class="m-2"/>
+    </a>
+</header>
+<main>
+    {@render children()}
+</main>
---
+++ web-ui/src/routes/login/+page.svelte
@@ -0,0 +1,24 @@
+<div class="py-32 flex items-center justify-center">
+    <div class="card preset-filled-surface-100-900 border-[1px] border-surface-200-800 card-hover
+                divide-surface-200-800 block divide-y w-128">
+        <header class="space-y-4 p-4 font-bold">
+            LOGIN
+        </header>
+        <article class="space-y-4 p-4">
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+            <p>this i sthe  sdonca</p>
+        </article>
+    </div>
+</div>
