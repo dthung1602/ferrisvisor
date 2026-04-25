6522dc405633bac9ae4f51ca7b76273807e090ad
henry.duong <henry.duong@grasshopperasia.com>
2026-04-25 21:16:18 +0700

    Error route

---
+++ web-ui/src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { page } from "$app/state";
+
+  // eslint-disable-next-line svelte/no-navigation-without-resolve
+  goto(`/error?code=${page.status}`);
+</script>
