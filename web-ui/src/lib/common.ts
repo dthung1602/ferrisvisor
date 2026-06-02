28b6a3809325edcfc21640e8be68e92ef298a34d
henry.duong <henry.duong@grasshopperasia.com>
2026-06-02 23:04:58 +0700

    Add setting page

--- web-ui/src/lib/common.ts
+++ web-ui/src/lib/common.ts
@@ -7,11 +7,11 @@ export function formatDate(date: string) {
   const instant = window.Temporal.Instant.from(date);
   const dt = instant.toZonedDateTimeISO(timeZone);
   const year = dt.year;
-  const month = String(dt.month).padStart(2, '0');
-  const day = String(dt.day).padStart(2, '0');
-  const hour = String(dt.hour).padStart(2, '0');
-  const minute = String(dt.minute).padStart(2, '0');
-  const second = String(dt.second).padStart(2, '0');
+  const month = String(dt.month).padStart(2, "0");
+  const day = String(dt.day).padStart(2, "0");
+  const hour = String(dt.hour).padStart(2, "0");
+  const minute = String(dt.minute).padStart(2, "0");
+  const second = String(dt.second).padStart(2, "0");
   return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
 }
 
